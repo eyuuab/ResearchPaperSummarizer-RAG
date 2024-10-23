19914d9a1f70aaffa9fe60cd52aec0ee7fb1473e
eyobed <eyobedabreham9@gmail.com>
2024-10-23 10:53:08 +0300

    fix:add constract

--- extract.py
+++ extract.py
@@ -1,11 +1,11 @@
-import fitz
+import fitz  # PyMuPDF
 import os
 
 def extract_text(path):
     pdf = fitz.open(path)
     text = ''
     for page in pdf:
-        text +=page.get_text()
+        text += page.get_text()
     return text
 
 def extract_text_from_directory(directory):
@@ -14,12 +14,15 @@ def extract_text_from_directory(directory):
         if file.endswith('.pdf'):
             path = os.path.join(directory, file)
             text += extract_text(path)
+            print(f"Extracted text from {file}")
     return text
 
-directory = 'data'
-extracted_text = extract_text_from_directory(directory)
-
-
-output_file_path = os.path.join(directory, 'extracted_text.txt')
-with open(output_file_path, 'w') as file:
-    file.write(extracted_text)
+if __name__ == "__main__":
+    directory = 'data'  
+    extracted_text = extract_text_from_directory(directory)
+    
+    output_file_path = os.path.join('data', 'extracted_text.txt')
+    with open(output_file_path, 'w') as file:
+        file.write(extracted_text)
+    
+    print(f"Text extraction completed and saved to {output_file_path}")
